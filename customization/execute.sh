1e230403914d216661c47af97b7d09dc150e025c
Steven Mirabito <steven@stevenmirabito.com>
2017-12-17 20:49:23 -0500

    Also update HA config

--- customization/execute.sh
+++ customization/execute.sh
@@ -4,4 +4,4 @@ JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 
 echo "=> Executing customization script"
 $JBOSS_CLI --file=$JBOSS_HOME/customization/custom.cli
-
+$JBOSS_CLI --file=$JBOSS_HOME/customization/custom-ha.cli
